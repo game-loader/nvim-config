e1ecbd981817e3638481cd581992f16f9539bc42
gameloader <ggwqqo@163.com>
2022-10-18 23:30:26 +0800

    change toggleterm size

--- lua/plugin-config/toggleterm.lua
+++ lua/plugin-config/toggleterm.lua
@@ -1,4 +1,11 @@
 require("toggleterm").setup({
+	size = function(term)
+		if term.direction == "horizontal" then
+			return vim.o.lines * 0.3
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
 	open_mapping = [[<C-\>]],
 	insert_mapping = true,
 	start_in_insert = true,
