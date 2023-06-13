7fe0aa35629d51c167e9e1c833ae08011b09cf71
gameloader <ggwqqo@163.com>
2023-06-13 20:04:09 +0800

    update install script

--- install.sh
+++ install.sh
@@ -16,19 +16,22 @@ fi
 
 # Install packages
 if [[ "$OS" == "macos" ]]; then
-  brew install unzip wget curl gzip tar ruby go node python git cargo npm
+  brew install unzip wget curl gzip tar ruby go node python git cargo npm fzf
 elif [[ "$OS" == "debian" ]] || [[ "$OS" == "ubuntu" ]]; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable
   sudo apt-get update
-  sudo apt-get install -y unzip wget curl gzip tar ruby git cargo npm
-  sudo apt-get install -y python3 python3-pip node
-  curl -LO https://github.com/neovim/neovim/releases/download/v0.9.1/nvim-linux64.deb # Added this line to download neovim deb package
-  sudo apt install ./nvim-linux64.deb # Added this line to install neovim deb package
+  sudo apt-get install -y unzip wget curl gzip tar ruby git cargo npm xclip fzf
+  sudo apt-get install -y python3 python3-venv python3-pip node neovim
 elif [[ "$OS" == "manjaro" ]]; then
   sudo pacman -Syu --noconfirm
   sudo pacman -S --noconfirm unzip wget curl gzip tar ruby git cargo npm
-  sudo pacman -S --noconfirm python python-pip
+  sudo pacman -S --noconfirm python python-pip fzf
   sudo pacman -S --noconfirm go nodejs neovim
 fi
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 
 # Add Go to PATH
 if [[ "$OS" == "macos" ]]; then
@@ -36,6 +39,8 @@ if [[ "$OS" == "macos" ]]; then
   exit 0
 elif [[ "$OS" == "debian" ]] || [[ "$OS" == "ubuntu" ]] || [[ "$OS" == "manjaro" ]]; then
   GO_LATEST=$(curl -sL https://golang.org/VERSION?m=text | sed 's/go//')
-  curl -s https://mirrors.aliyun.com/golang/go$GO_LATEST.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
+  curl  https://mirrors.aliyun.com/golang/go$GO_LATEST.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
   echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
 fi
+
+source ~/.bashrc
